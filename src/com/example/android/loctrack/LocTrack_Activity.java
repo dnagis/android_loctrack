21300f900b9b299328deb2d7486894f9f66fe3a0
dnagis <vincent.achard@gmail.com>
2019-11-03 10:34:08 +0100

    GPS premiers retours logcat

--- src/com/example/android/loctrack/LocTrack_Activity.java
+++ src/com/example/android/loctrack/LocTrack_Activity.java
@@ -4,6 +4,9 @@
 adb uninstall com.example.android.loctrack
 adb install out/target/product/mido/system/app/LocTrack/LocTrack.apk
 
+
+pm grant com.example.android.loctrack android.permission.ACCESS_FINE_LOCATION
+
  */
 
 package com.example.android.loctrack;
@@ -16,12 +19,20 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.util.Log;
 
+import android.content.Context;
+import android.content.Intent;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 
 
 
-public class LocTrack_Activity extends Activity {
+public class LocTrack_Activity extends Activity implements LocationListener {
 	
-
+	public LocationManager mLocationManager;		
+	private static final int MIN_TIME = 10 * 1000; //long: minimum time interval between location updates, in milliseconds
+    private static final int MIN_DIST = 0; //float: minimum distance between location updates, in meters
 
     /**
      * Called with the activity is first created.
@@ -30,19 +41,51 @@ public class LocTrack_Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Set the layout for this activity.  You can find it
-        // in res/layout/hello_activity.xml
+        // Set the layout for this activity.  You can find it res/layout/hello_activity.xml
         View view = getLayoutInflater().inflate(R.layout.hello_activity, null);
         setContentView(view);
         
         final Button button = findViewById(R.id.button_id);
+        
+        
+        
+        mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DIST, this);
+		
+		
+		//foreground service pour importance (am package-importance com.example.android.hellogps) à 125
+		startForegroundService(new Intent(this, ForegroundService.class));
 
 
     }
     
     public void ActionPressBouton(View v) {
-		Log.d("LocTrack", "press bouton hello world");
+		Log.d("LocTrack", "press bouton");
+	}
+	
+	
+		/**
+	 *
+	 * MainActivity implements LocationListener --> il faut les 4 méthodes 
+	 * 
+	 * 
+	 **/    
+    @Override	
+    public void onLocationChanged(Location location) {
+        Log.d("LocTrack", location.getLatitude() + ",  " + location.getLongitude() + ",  " + location.getAccuracy() + ",  " + location.getAltitude() + ",  " + location.getTime());
+        //maBDD.logFix(location.getTime()/1000, location.getLatitude(), location.getLongitude(), location.getAccuracy(), location.getAltitude());   
+    }
+        
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
 
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
 	}
 }
 
